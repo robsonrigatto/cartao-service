9e2adb70e4a1414eebe6a1d56b53ce1cc73d741d
robsonrigatto <rigatto.robson@gmail.com>
2020-03-13 09:48:44 -0300

    hystrix dependency + ribbon configuration

--- src/main/java/br/com/rr/mastertech/cartao/CartoesServiceApplication.java
+++ src/main/java/br/com/rr/mastertech/cartao/CartoesServiceApplication.java
@@ -1,11 +1,16 @@
 package br.com.rr.mastertech.cartao;
 
+import br.com.rr.mastertech.cartao.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@EnableCircuitBreaker
 public class CartoesServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/rr/mastertech/cartao/client/ClienteClient.java
+++ src/main/java/br/com/rr/mastertech/cartao/client/ClienteClient.java
@@ -4,12 +4,10 @@ import br.com.rr.mastertech.cartao.client.dto.ClienteDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "cliente")
-@RequestMapping("/cliente")
+@FeignClient(name = "cliente", fallbackFactory = ClienteClientFallbackFactory.class)
 public interface ClienteClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/cliente/{id}")
     ClienteDTO findById(@PathVariable Integer id);
 }

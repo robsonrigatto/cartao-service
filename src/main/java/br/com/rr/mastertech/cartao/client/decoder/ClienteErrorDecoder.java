2fef3849eaa01f77b9cc93b0640ebaa6476c863d
robsonrigatto <rigatto.robson@gmail.com>
2020-03-13 14:43:27 -0300

    fixing compilation

--- src/main/java/br/com/rr/mastertech/cartao/client/decoder/ClienteErrorDecoder.java
+++ src/main/java/br/com/rr/mastertech/cartao/client/decoder/ClienteErrorDecoder.java
@@ -1,11 +1,11 @@
 package br.com.rr.mastertech.cartao.client.decoder;
 
-import br.com.rr.mastertech.cartao.client.exception.ClienteErroInesperadoException;
-import br.com.rr.mastertech.cartao.exception.ClienteNaoEncontradoException;
-import feign.Response;
-import feign.codec.ErrorDecoder;
-import org.springframework.http.HttpStatus;
-
+//import br.com.rr.mastertech.cartao.client.exception.ClienteErroInesperadoException;
+//import br.com.rr.mastertech.cartao.exception.ClienteNaoEncontradoException;
+//import feign.Response;
+//import feign.codec.ErrorDecoder;
+//import org.springframework.http.HttpStatus;
+//
 //public class ClienteErrorDecoder implements ErrorDecoder {
 //
 //    @Override
--- src/main/java/br/com/rr/mastertech/cartao/configuration/ClienteClientConfiguration.java
+++ src/main/java/br/com/rr/mastertech/cartao/configuration/ClienteClientConfiguration.java
@@ -1,10 +1,10 @@
 package br.com.rr.mastertech.cartao.configuration;
 
-import br.com.rr.mastertech.cartao.client.decoder.ClienteErrorDecoder;
-import feign.codec.ErrorDecoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+//import br.com.rr.mastertech.cartao.client.decoder.ClienteErrorDecoder;
+//import feign.codec.ErrorDecoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
 //@Configuration
 //public class ClienteClientConfiguration {
 //
